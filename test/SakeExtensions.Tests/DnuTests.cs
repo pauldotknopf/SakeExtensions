6a8b344ea9beeeda9226bf0e786cba75dd815a70
Paul Knopf <theonlylawislove@gmail.com>
2015-11-30 00:44:49 -0500

    Added a dnu restore command.

---
+++ test/SakeExtensions.Tests/DnuTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SakeExtensions.Tests
+{
+    public class DnuTests : SandboxBase
+    {
+        [Fact]
+        public void Can_build()
+        {
+            Directory.CreateDirectory(SandboxPath("testdirectory"));
+            File.WriteAllText(SandboxPath("testdirectory", "project.json"), @"
+{
+  ""version"": ""1.0.0-*"",
+
+  ""frameworks"": {
+    ""dnx451"": { }
+  }
+}
+");
+
+            var result = RunShadeDnx(@"
+var VERSION='0.1'
+var FULL_VERSION='0.1'
+var AUTHORS='Paul Knopf'
+
+use import=""Common""
+
+#default
+    dnu-restore restoreDir='testdirectory'
+    dnu-build projectFile='testdirectory'
+");
+
+            Assert.True(result.Contains("Build succeeded."));
+        }
+
+        //[Fact]
+        public void Can_restore()
+        {
+            Directory.CreateDirectory(SandboxPath("testdirectory"));
+            File.WriteAllText(SandboxPath("testdirectory", "project.json"), @"
+{
+  ""version"": ""1.0.0-*"",
+
+  ""frameworks"": {
+    ""dnx451"": { },
+    ""dnxcore50"": { }
+  }
+}
+");
+
+            RunShadeDnx(@"
+var VERSION='0.1'
+var FULL_VERSION='0.1'
+var AUTHORS='Paul Knopf'
+
+use import=""Common""
+
+#default
+    dnu-restore restoreDir='testdirectory'");
+
+            Assert.True(File.Exists(SandboxPath("testdirectory", "project.lock.json")));
+        }
+    }
+}
--- test/SakeExtensions.Tests/SandboxBase.cs
+++ test/SakeExtensions.Tests/SandboxBase.cs
@@ -84,6 +84,69 @@ namespace SakeExtensions.Tests
             return result.ToString();
         }
 
+        protected string RunShadeDnx(string shade)
+        {
+            // install dnvm first
+            RunShade(@"
+use import='Common'
+
+#default
+    dnvm-install
+");
+
+            var result = new StringBuilder();
+
+            var temp = Guid.NewGuid().ToString().Replace("-", "") + ".shade";
+
+            File.WriteAllText(Path.Combine(_sandBoxDirectory, temp), shade);
+
+            var dnvmUseCommand = @"bin\dnvm\dnvm use default";
+            var sakeCommand = string.Format("\"" + _sakeExePath + "\" -I \"{0}\" -I \"{1}\" -f \"{2}\"", Path.Combine(_sakeDirectory, "Shared"), _buildScriptsDirectory, temp);
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                WorkingDirectory = _sandBoxDirectory,
+                FileName = "cmd",
+                Arguments = string.Format("/C {0}", dnvmUseCommand + " && " + sakeCommand)
+            };
+
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.RedirectStandardOutput = true;
+
+            using (var process = Process.Start(processStartInfo))
+            {
+                process.EnableRaisingEvents = true;
+
+                process.ErrorDataReceived += (sender, eventArgs) =>
+                {
+                    if (!string.IsNullOrWhiteSpace(eventArgs.Data))
+                    {
+                        Console.WriteLine(eventArgs.Data);
+                        result.AppendLine(eventArgs.Data);
+                    }
+                };
+
+                process.OutputDataReceived += (sender, eventArgs) =>
+                {
+                    Console.WriteLine(eventArgs.Data);
+                    result.AppendLine(eventArgs.Data);
+                };
+
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                process.WaitForExit();
+
+                if (process.ExitCode != 0)
+                {
+                    throw new Exception(string.Format("Exit code {0} from {1}", process.ExitCode, "sake"));
+                }
+            }
+
+            return result.ToString();
+        }
+
         protected string RunShadeFile(string file)
         {
             if (!file.EndsWith(".shade"))
