06e7e3630303d6a04d26378e03b9284b5e057f2f
Paul Knopf <theonlylawislove@gmail.com>
2015-12-01 04:03:48 -0500

    Added support for running tests.

---
+++ test/SakeExtensions.Tests/DnxTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SakeExtensions.Tests
+{
+    public class DnxTests : SandboxBase
+    {
+        [Fact]
+        public void Can_run_tests()
+        {
+            Directory.CreateDirectory(SandboxPath("test"));
+
+            File.WriteAllText(SandboxPath("test", "project.json"), @"
+{
+  ""version"": ""1.0.0-*"",
+
+  ""dependencies"": {
+    ""xunit"": ""2.1.0-*"",
+    ""xunit.assert"": ""2.1.0-*"",
+    ""xunit.runner.dnx"": ""2.1.0-*""
+  },
+
+  ""commands"": {
+    ""test"": ""xunit.runner.dnx""
+  },
+
+  ""frameworks"": {
+    ""dnx451"": { }
+  }
+}
+");
+
+            File.WriteAllText(SandboxPath("test", "Test.cs"), @"
+using Xunit;
+
+public class Test
+{
+    [Fact]
+    public void Can_do_something()
+    {
+        Assert.True(true);
+    }
+}
+");
+
+            var result = RunShadeDnx(@"
+var VERSION='0.1'
+var FULL_VERSION='0.1'
+var AUTHORS='Paul Knopf'
+
+use import=""Common""
+
+#default
+    dnu-restore restoreDir='${Path.Combine(Directory.GetCurrentDirectory(), ""test"")}'
+    dnx-test dnxDir='test' projectFile='${Path.Combine(Directory.GetCurrentDirectory(), ""test"", ""project.json"")}'
+");
+
+            Assert.True(result.Contains("Total: 1, Errors: 0, Failed: 0, Skipped: 0"));
+        }
+    }
+}
