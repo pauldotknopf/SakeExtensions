59dd1ae5877dc30a8ba91afd7d256ab8a7def851
Paul Knopf <theonlylawislove@gmail.com>
2015-12-01 05:02:29 -0500

    Added support for dnvm use for the entire make process.

--- test/SakeExtensions.Tests/DnvmTests.cs
+++ test/SakeExtensions.Tests/DnvmTests.cs
@@ -19,5 +19,29 @@ use import='Common'
             Assert.True(SandboxFileExists("bin", "dnvm", "dnvm.ps1"));
             Assert.True(SandboxFileExists("bin", "dnvm", "dnvm.sh"));
         }
+
+        [Fact]
+        public void Can_use_runtime()
+        {
+            RunShade(@"
+use import='Common'
+
+#default
+    dnvm-install
+    dnvm-use runtime='CLR'
+    exec-command commandLine='dnx --version > output1.txt'
+    dnvm-use runtime='CoreCLR'
+    exec-command commandLine='dnx --version > output2.txt'
+    dnvm-use runtime='CLR'
+    exec-command commandLine='dnx --version > output3.txt'
+");
+
+            Assert.True(File.Exists(SandboxPath("output1.txt")));
+            Assert.True(File.ReadAllText(SandboxPath("output1.txt")).Contains("Type:         Clr"));
+            Assert.True(File.Exists(SandboxPath("output2.txt")));
+            Assert.True(File.ReadAllText(SandboxPath("output2.txt")).Contains("Type:         CoreClr"));
+            Assert.True(File.Exists(SandboxPath("output3.txt")));
+            Assert.True(File.ReadAllText(SandboxPath("output3.txt")).Contains("Type:         Clr"));
+        }
     }
 }
