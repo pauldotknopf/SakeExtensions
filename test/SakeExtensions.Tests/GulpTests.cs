08b84c30b098d00813e03f8610503b14a81377c5
Paul Knopf <theonlylawislove@gmail.com>
2015-12-02 03:34:59 -0500

    Added support for installing gulp and running gulp commands.

---
+++ test/SakeExtensions.Tests/GulpTests.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Xunit;
+
+namespace SakeExtensions.Tests
+{
+    public class GulpTests : SandboxBase
+    {
+        [Fact]
+        public void Can_install_gulp()
+        {
+            File.WriteAllText(SandboxPath("gulpfile.js"), @"
+var gulp = require('gulp');
+
+gulp.task('ci', function() {
+  console.log('test output from gulp.js');
+});
+");
+
+            var result = RunShade(@"
+var VERSION='0.1'
+var FULL_VERSION='0.1'
+var AUTHORS='Paul Knopf'
+
+use import='Common'
+
+#default
+    gulp-install
+    gulp gulpCommand='ci'
+");
+
+            Assert.True(result.Contains("test output from gulp.js"));
+        }
+    }
+}
